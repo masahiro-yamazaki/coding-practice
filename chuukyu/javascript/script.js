a97ea06bdcc42c86a5a84809ccd85e10941c4d4c
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-06-18 23:59:59 +0900

    ハンバーガーメニューのJSを修正

--- chuukyu/javascript/script.js
+++ chuukyu/javascript/script.js
@@ -1,6 +1,17 @@
 $(function () {
   // ハンバーガーメニュー
   $('.js-header-menu-button').click(function () {
-    $('.js-hamburgar-menu-sp').toggleClass('show');
+    $('body').toggleClass('is-drawerActive');
+
+    //attr()でaria-expandedがfalseの場合、thisはクリックした要素そのもの
+    if ($(this).attr('aria-expanded') == 'false') {
+      $(this).attr('aria-expanded', true);
+      $('.js-hamburgar-menu-sp').css('visibility', 'visible');
+      $('.js-hamburgar-menu-sp').attr('area-hidden', 'false');
+    } else {
+      $(this).attr('aria-expanded', false);
+      $('.js-hamburgar-menu-sp').css('visibility', 'hidden');
+      $('.js-hamburgar-menu-sp').attr('area-hidden', 'true');
+    }
   });
 });
