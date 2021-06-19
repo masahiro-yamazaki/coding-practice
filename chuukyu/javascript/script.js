b2f7618815d09e986ebfae2b4ef90d5c1a799ccc
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-06-19 12:26:45 +0900

    メニューが徐々に表示されるロジックを作成

--- chuukyu/javascript/script.js
+++ chuukyu/javascript/script.js
@@ -6,11 +6,11 @@ $(function () {
     //attr()でaria-expandedがfalseの場合、thisはクリックした要素そのもの
     if ($(this).attr('aria-expanded') == 'false') {
       $(this).attr('aria-expanded', true);
-      $('.js-hamburgar-menu-sp').css('visibility', 'visible');
+      $('.js-hamburgar-menu-sp').addClass('show');
       $('.js-hamburgar-menu-sp').attr('aria-hidden', 'false');
     } else {
       $(this).attr('aria-expanded', false);
-      $('.js-hamburgar-menu-sp').css('visibility', 'hidden');
+      $('.js-hamburgar-menu-sp').removeClass('show');
       $('.js-hamburgar-menu-sp').attr('aria-hidden', 'true');
     }
   });
