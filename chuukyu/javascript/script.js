bcd1f79c2e94c37b1570434135790424b2e5a700
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-06-19 12:37:56 +0900

    JSのセレクタを変更

--- chuukyu/javascript/script.js
+++ chuukyu/javascript/script.js
@@ -1,17 +1,17 @@
 $(function () {
   // ハンバーガーメニュー
-  $('.js-header-menu-button').click(function () {
+  $('#js-menu-button').click(function () {
     $('body').toggleClass('is-drawerActive');
 
     //attr()でaria-expandedがfalseの場合、thisはクリックした要素そのもの
     if ($(this).attr('aria-expanded') == 'false') {
       $(this).attr('aria-expanded', true);
-      $('.js-hamburgar-menu-sp').addClass('show');
-      $('.js-hamburgar-menu-sp').attr('aria-hidden', 'false');
+      $('#js-glabal-menu').addClass('show');
+      $('#js-glabal-menu').attr('aria-hidden', 'false');
     } else {
       $(this).attr('aria-expanded', false);
-      $('.js-hamburgar-menu-sp').removeClass('show');
-      $('.js-hamburgar-menu-sp').attr('aria-hidden', 'true');
+      $('#js-glabal-menu').removeClass('show');
+      $('#js-glabal-menu').attr('aria-hidden', 'true');
     }
   });
 });
