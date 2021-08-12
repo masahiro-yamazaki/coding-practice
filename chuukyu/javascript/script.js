bcc58d875c786523366ea55cdea08aa7205fe7bb
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-08-12 19:09:46 +0900

    AOSを導入

--- chuukyu/javascript/script.js
+++ chuukyu/javascript/script.js
@@ -1,4 +1,14 @@
 $(function () {
+  // AOS
+  AOS.init({
+    offset: 100,
+    delay: 200,
+    duration: 500,
+    easing: 'ease-out',
+    anchorPlacement: 'bottom-top',
+    once: true
+  });
+
   // ハンバーガーメニュー
   $('#js-menu-button').click(function () {
     $('body').toggleClass('is-drawerActive');
