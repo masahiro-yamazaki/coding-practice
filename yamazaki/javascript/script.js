3f81fd584c6c48db431360eea108b12afcd4a717
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-09-18 22:44:07 +0900

    AOS.jsを追加

---
+++ yamazaki/javascript/script.js
@@ -0,0 +1,11 @@
+$(function () {
+  // AOS
+  AOS.init({
+    offset: 100,
+    delay: 200,
+    duration: 500,
+    easing: 'ease-out',
+    anchorPlacement: 'bottom-top',
+    once: true
+  });
+});
