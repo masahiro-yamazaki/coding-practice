89c0d3f6218919c78c4b1297ca84a55bf5285a3e
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-10-09 19:24:57 +0900

    feat: ハンバーガーメニューを開くボタンと閉じるボタンのアイコンを分ける対応

--- ishii-kadan/javascript/script.js
+++ ishii-kadan/javascript/script.js
@@ -3,5 +3,7 @@ $(function () {
   $('#js-hmenu-button').click(function () {
     $('body').toggleClass('no-scroll');
     $('.hmenu').toggleClass('show');
+    $('.js-hmenu-open-icon').toggleClass('show');
+    $('.js-hmenu-close-icon').toggleClass('show');
   });
 });
