dab03b51b50a3e02504206e16772943441884f9f
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-10-09 15:12:56 +0900

    feat: 下層ページでもハンバーガーメニューのボタンを表示

--- ishii-kadan/javascript/script.js
+++ ishii-kadan/javascript/script.js
@@ -6,11 +6,13 @@ $(function () {
       $('.header-menu').removeClass('header-menu--white');
       $('.header-image--default').css('display', 'none');
       $('.header-image--scrolled').css('display', 'block');
+      $('.header-hmenu-button').removeClass('header-hmenu-button--white');
     } else {
       $('.header').addClass('header--black');
       $('.header-menu').addClass('header-menu--white');
       $('.header-image--default').css('display', 'block');
       $('.header-image--scrolled').css('display', 'none');
+      $('.header-hmenu-button').addClass('header-hmenu-button--white');
     }
   });
 });
