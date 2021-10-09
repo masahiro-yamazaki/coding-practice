3a3a88d29b9823dfbe8f3b3e0418ab4a7c527fd9
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-10-09 20:32:34 +0900

    feat: ハンバーガーメニューにaria属性を追加

--- ishii-kadan/javascript/script.js
+++ ishii-kadan/javascript/script.js
@@ -5,5 +5,14 @@ $(function () {
     $('.hmenu').toggleClass('show');
     $('.js-hmenu-open-icon').toggleClass('show');
     $('.js-hmenu-close-icon').toggleClass('show');
+
+    //attr()でaria-expandedがfalseの場合、thisはクリックした要素そのもの
+    if ($(this).attr('aria-expanded') == 'false') {
+      $(this).attr('aria-expanded', true);
+      $('.hmenu').attr('aria-hidden', 'false');
+    } else {
+      $(this).attr('aria-expanded', false);
+      $('.hmenu').attr('aria-hidden', 'true');
+    }
   });
 });
