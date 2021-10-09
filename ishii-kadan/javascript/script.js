dd871e40991c29bc3340d1835ac636b6d4252457
masahiro-yamazaki <masahiro.yamazaki@stepdays.com>
2021-10-09 17:26:20 +0900

    feat: ハンバーガーメニューを表示

--- ishii-kadan/javascript/script.js
+++ ishii-kadan/javascript/script.js
@@ -1,21 +1,4 @@
 $(function () {
-  // トップページのヘッダースクロール時に背景色を変更
-  $(window).on('scroll', function () {
-    if ($('.header').height() < $(this).scrollTop()) {
-      $('.header').removeClass('header--black');
-      $('.header-menu').removeClass('header-menu--white');
-      $('.header-image--default').css('display', 'none');
-      $('.header-image--scrolled').css('display', 'block');
-      $('.header-hmenu-button').removeClass('header-hmenu-button--white');
-    } else {
-      $('.header').addClass('header--black');
-      $('.header-menu').addClass('header-menu--white');
-      $('.header-image--default').css('display', 'block');
-      $('.header-image--scrolled').css('display', 'none');
-      $('.header-hmenu-button').addClass('header-hmenu-button--white');
-    }
-  });
-
   // ハンバーガーメニュー
   $('#js-hmenu-button').click(function () {
     $('body').toggleClass('no-scroll');
